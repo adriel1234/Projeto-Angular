3405b052df54af23aa680301e3599b94a8c1b295
Adriel Vinicius <adriel1234.1234@gmail.com>
2023-04-14 13:43:32 -0400

    commit

---
+++ src/app/components/menu.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'menu-component',
+  template: '<h2>Adriel</h2>',
+  styles: ['h2{color:blue}'],
+})
+export class MenuComponent {}
